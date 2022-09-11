6e653a96fbd4d49e39a820c8d9a56b135fca6167
Joel Martinez <joel.esli@gmail.com>
2022-09-11 10:14:08 +0200

    First commit: created project

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Joel Martinez on 9/5/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
